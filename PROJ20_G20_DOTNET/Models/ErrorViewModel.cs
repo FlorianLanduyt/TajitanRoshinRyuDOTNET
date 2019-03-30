0a4b3174d6414d6fac5cf2354ccdf0ca48e70d5d
Tybo Vanderstraeten <tybo.vanderstraeten@student.hogent.be>
2019-03-30 15:02:47 +0100

    Add project files

---
+++ PROJ20_G20_DOTNET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PROJ20_G20_DOTNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
