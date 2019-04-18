4cf345b2bc5579840695eb8b7f70dff1b9e46f78
Rob De Putter <rob.deputter@student.hogent.be>
2019-04-18 21:26:38 +0200

    domeinvalidatie van inschrijving en thema (voor raadpleging is er geen validatie)

--- PROJ20_G20_DOTNET/Models/Domain/Inschrijving.cs
+++ PROJ20_G20_DOTNET/Models/Domain/Inschrijving.cs
@@ -1,25 +1,52 @@
 ﻿using System;
 
-namespace PROJ20_G20_DOTNET.Models.Domain {
-    public class Inschrijving {
+namespace PROJ20_G20_DOTNET.Models.Domain
+{
+    public class Inschrijving
+    {
+
+        private Lid _lid;
+        private DateTime _tijdstip;
 
         #region Properties
-        public Lid Lid { get; set; }
+        public Lid Lid {
+            get {
+                return _lid;
+            }
+            set {
+                _lid = value ?? throw new ArgumentException("Lid mag niet leeg zijn.");
+            }
+        }
+
         public int LidId { get; set; } // voor mapping met juiste column name
+
         public Formule Formule { get; set; }
-        public DateTime Tijdstip { get; set; }
+        public DateTime Tijdstip {
+            get {
+                return _tijdstip;
+            }
+            set {
+                if(value == null)
+                {
+                    throw new ArgumentException("Tijdstip mag niet leeg zijn.");
+                }
+                _tijdstip = value;
+            }
+        }
         public int Id { get; set; }
         #endregion
 
         #region Constructors
-        protected Inschrijving() {
+        protected Inschrijving()
+        {
         }
 
-        public Inschrijving(Lid lid, Formule formule, DateTime tijdstip) {
+        public Inschrijving(Lid lid, Formule formule, DateTime tijdstip)
+        {
             Lid = lid;
             Formule = formule;
             Tijdstip = tijdstip;
-        } 
+        }
         #endregion
     }
 }
--- PROJ20_G20_DOTNET/Models/Domain/Thema.cs
+++ PROJ20_G20_DOTNET/Models/Domain/Thema.cs
@@ -1,15 +1,59 @@
-﻿namespace PROJ20_G20_DOTNET.Models.Domain {
-    public class Thema {
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PROJ20_G20_DOTNET.Models.Domain
+{
+    public class Thema
+    {
+
+        private string _naam;
 
         #region Properties
-        public string Naam { get; set; }
+        public string Naam {
+            get {
+                return _naam;
+            }
+
+            set {
+                if (value != null)
+                {
+                    string naam = value.Trim();
+                    if (naam.Length > 20) // 
+                    {
+                        throw new ArgumentException("Naam mag max. 20 karakters bevatten");
+                    }
+                    if (naam.Contains(" "))
+                    {
+                        string tempnaam = naam.Replace(" ", "");
+                        if(Regex.Match(tempnaam, ".*[\\d\\W].*").Success)
+                        {
+                            throw new ArgumentException("Naam mag enkel letters bevatten.");
+                        }
+
+                    }
+                    else
+                    {
+                        if (Regex.Match(value, ".*[\\d\\W].*").Success)
+                        {
+                            throw new ArgumentException("Naam mag enkel letters bevatten.");
+                        }
+                    }
+                    _naam = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Naam mag niet leeg zijn.");
+                }
+            }
+        }
         public int Id { get; set; }
         #endregion
 
         #region Properties
         protected Thema() { }
 
-        public Thema(string naam) {
+        public Thema(string naam)
+        {
             Naam = naam;
         }
 
