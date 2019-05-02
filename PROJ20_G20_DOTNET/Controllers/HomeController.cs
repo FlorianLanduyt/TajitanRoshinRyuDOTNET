81fe5c5f7eb7ffc188cca9f90ab9fe02e41b0138
Florian Landuyt <florian.landuyt.y2414@student.hogent.be>
2019-05-02 19:55:16 +0200

    Homepage style

--- PROJ20_G20_DOTNET/Controllers/HomeController.cs
+++ PROJ20_G20_DOTNET/Controllers/HomeController.cs
@@ -7,15 +7,46 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PROJ20_G20_DOTNET.Models.Domain;
 
-namespace PROJ20_G20_DOTNET.Controllers
-{
+namespace PROJ20_G20_DOTNET.Controllers {
     [AllowAnonymous]
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
+    public class HomeController : Controller {
+        private readonly ILidRepository _lidRepository;
+        private readonly IAanwezigheidRepository _aanwezigheidRepository;
+
+        public HomeController(ILidRepository lidRepository, 
+            IAanwezigheidRepository aanwezigheidRepository) {
+            _lidRepository = lidRepository;
+            _aanwezigheidRepository = aanwezigheidRepository;
+        }
+
+        public IActionResult Index() {
+            IList<string> lijst = new List<string> {
+                "Profiel",
+                "Aanwezigheden",
+                "Lesmateriaal"
+            };
+            return View(lijst);
+        }
+
+        public IActionResult Profiel() {
+            string user = Environment.UserName;
+            _lidRepository.GetAll().SingleOrDefault(l => l.Voornaam.StartsWith(user));
+
             return View();
         }
 
+        public IActionResult Aanwezigheden(int id) {
+            return View();
+        }
+
+        public IActionResult LesMateriaal(int id) {
+            return View();
+        }
+
+
+
     }
+
+
+
 }
