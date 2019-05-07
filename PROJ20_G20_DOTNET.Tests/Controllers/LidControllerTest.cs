b7a2d7934b9310226fcbcee4a85b86ecdae5cff0
Rob De Putter <rob.deputter@student.hogent.be>
2019-05-07 23:32:11 +0200

    Constructor en fields voor LidControllerTest

--- PROJ20_G20_DOTNET.Tests/Controllers/LidControllerTest.cs
+++ PROJ20_G20_DOTNET.Tests/Controllers/LidControllerTest.cs
@@ -1,9 +1,18 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
 using PROJ20_G20_DOTNET.Controllers;
 using PROJ20_G20_DOTNET.Models.Domain;
+using PROJ20_G20_DOTNET.Models.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace PROJ20_G20_DOTNET.Tests.Controllers
 {
@@ -11,14 +20,57 @@ namespace PROJ20_G20_DOTNET.Tests.Controllers
     {
         private LidController _controller;
         private Mock<ILidRepository> _lidRepository;
-        
+        private DummyJiuJitsuDbContext _dummyContext;
+        private Mock<UserManager<IdentityUser>> _userManager;
+        private Mock<SignInManager<IdentityUser>> _signInManager;
+
+        private ClaimsPrincipal user;
+        private IdentityUser identityUser;
+        private IdentityUser wrongIdentityUser;
 
         public LidControllerTest()
         {
+            _dummyContext = new DummyJiuJitsuDbContext();
             _lidRepository = new Mock<ILidRepository>();
-            //_controller = new LidController(_lidRepository.Object) Hoe kan je de Usermanager ne SignInManager mocken ?
-            
+            _userManager = new Mock<UserManager<IdentityUser>>(
+                new Mock<IUserStore<IdentityUser>>().Object,
+              new Mock<IOptions<IdentityOptions>>().Object,
+              new Mock<IPasswordHasher<IdentityUser>>().Object,
+              new IUserValidator<IdentityUser>[0],
+              new IPasswordValidator<IdentityUser>[0],
+              new Mock<ILookupNormalizer>().Object,
+              new Mock<IdentityErrorDescriber>().Object,
+              new Mock<IServiceProvider>().Object,
+              new Mock<ILogger<UserManager<IdentityUser>>>().Object);
 
+            _signInManager = new Mock<SignInManager<IdentityUser>>(_userManager.Object,
+                     new Mock<IHttpContextAccessor>().Object,
+                     new Mock<IUserClaimsPrincipalFactory<IdentityUser>>().Object,
+                     new Mock<IOptions<IdentityOptions>>().Object,
+                     new Mock<ILogger<SignInManager<IdentityUser>>>().Object,
+                     new Mock<IAuthenticationSchemeProvider>().Object);
+
+            user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, _dummyContext.Rob.Email),
+            }));
+
+            identityUser = new IdentityUser() { Email = _dummyContext.Rob.Email };
+            wrongIdentityUser = new IdentityUser() { Email = _dummyContext.Tim.Email };
+
+
+            _controller = new LidController(_lidRepository.Object, _userManager.Object, _signInManager.Object)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = new DefaultHttpContext() { User = user }
+                }
+            };
         }
+
+        
     }
+
 }
+
+
